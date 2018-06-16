c04bc191a2313afaf37b90a3851a2d1a5a1a3fe4
k <joystats@yahoo.com>
2018-06-16 19:07:51 +0700

    set pass

--- wp-config.php
+++ wp-config.php
@@ -18,24 +18,45 @@
  * @package WordPress
  */
 
-// ** MySQL settings ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+if(file_exists(dirname(__FILE__).'/local.php')){
+	// ** MySQL settings ** //
+	/** The name of the database for WordPress */
+	define( 'DB_NAME', 'local' );
 
-/** MySQL database username */
-define( 'DB_USER', 'root' );
+	/** MySQL database username */
+	define( 'DB_USER', 'root' );
 
-/** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+	/** MySQL database password */
+	define( 'DB_PASSWORD', 'root' );
 
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+	/** MySQL hostname */
+	define( 'DB_HOST', 'localhost' );
 
-/** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+	/** Database Charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
 
-/** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+	/** The Database Collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+}else{
+	// ** MySQL settings ** //
+	/** The name of the database for WordPress */
+	define( 'DB_NAME', 'surachet_wordpress' );
+
+	/** MySQL database username */
+	define( 'DB_USER', 'surachet_wp238' );
+
+	/** MySQL database password */
+	define( 'DB_PASSWORD', '12345' );
+
+	/** MySQL hostname */
+	define( 'DB_HOST', 'localhost' );
+
+	/** Database Charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
+
+	/** The Database Collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+}
 
 /**
  * Authentication Unique Keys and Salts.
